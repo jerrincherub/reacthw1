3d97ad4dd2b6733ebe872756d99124a4e666dafc
jerrin cherub <jerrincherub@gmail.com>
2024-10-10 16:50:54 +0400

    calcoperation

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './index.css'
 
 import Thecalculator from "./components/Homework/calculator/calculator.jsx";
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Thecalculator />
